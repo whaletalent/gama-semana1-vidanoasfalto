650b31a180c48085f9c450ff91654103e15be8bc
Pedro Sato <pedro.hiro@gmail.com>
2019-06-17 14:31:21 -0300

    telas

--- easyCar/src/App.js
+++ easyCar/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './routes'
+import Header from './components/Header'
 
 
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes />
     </BrowserRouter>
   );
