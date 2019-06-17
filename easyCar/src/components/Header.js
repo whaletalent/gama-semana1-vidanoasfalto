7d09874387fdc8d22d839524ec82f59cb23e4b9a
LuanFerreira <luanfc@gmail.com>
2019-06-17 20:34:24 -0300

    deixando menu responsivo

--- easyCar/src/components/Header.js
+++ easyCar/src/components/Header.js
@@ -1,32 +1,36 @@
 import React from 'react';
-import './Header.css'
+import './Header.scss'
 import { Link } from 'react-router-dom'
+import { Navbar, Button } from 'rbx'
 
 
 export default function Header() {
     return (
-        <header id="main-header">
-            <div className="header-content">
-                <Link to='/'>
-                    <img src="" alt="Logo vida no asfalto" />
-                </Link>
-                <Link to='/'>
-                    <span className="link">Home</span>
-                </Link>
-                <Link to='/'>
-                    <span className="link">Artigos</span>
-                </Link>
-                <Link to='/quizz'>
-                    <span className="link">Quizz</span>
-                </Link>
-                <Link to='/sobre'>
-                    <span className="link">Sobre</span>
-                </Link>
-                <Link to='/contato'>
-                    <span className="link">Contato</span>
-                </Link>
-            </div>
-        </header>
+        <Navbar className="menu">
+        <Navbar.Brand>
+            <Navbar.Item href="#">
+            <img
+                src="https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/012013/cars-logo_0.png?itok=DfBpHAUz"
+                alt=""
+                role="presentation"
+                width="112"
+                height="28"
+            />
+            </Navbar.Item>
+            <Navbar.Burger className="menu-item"/>
+            
+        </Navbar.Brand>
+        <Navbar.Menu>
+            <Navbar.Segment align="start">
+                <Navbar.Item className="menu-item">Home</Navbar.Item>
+                <Navbar.Item className="menu-item">Artigos</Navbar.Item>
+                <Navbar.Item className="menu-item">Quizz</Navbar.Item>
+                <Navbar.Item className="menu-item">Sobre</Navbar.Item>
+                <Navbar.Item className="menu-item">Contato</Navbar.Item>
+                
+            </Navbar.Segment>
+        </Navbar.Menu>
+        </Navbar>
     );
 }
 
--- easyCar/src/index.js
+++ easyCar/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 //import 'bootstrap/dist/css/bootstrap.css';
 
-import './global.css'
+import './global.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
