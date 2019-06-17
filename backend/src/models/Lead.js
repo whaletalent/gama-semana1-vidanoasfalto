4254174670c1e1da246e14a5135c8253bb1cf9ef
seu nome <youremail@domain.com>
2019-06-16 23:25:49 -0300

    gravacao de nome e email no firebase

--- backend/src/models/Lead.js
+++ backend/src/models/Lead.js
@@ -1,9 +1,24 @@
 const firebase = require('../firebase')
-const create = ({ name }) => {
+const stringify = require('csv-stringify')
+
+const create = ({ name, email }) => {
     const leads = firebase.database().ref('leads')
-    const lead = leads.push({ name })
+    const lead = leads.push({ name, email })
     return lead
 }
+const csv = (cb) =>{
+    const leads = firebase.database().ref('leads')
+    const data =[['id', 'name', 'email']]
+    leads.on('value', (snapshot)=>{
+        snapshot.forEach((lead)=>{
+            const { name, email } = lead.val()
+            data.push([lead.key, name, email])
+        })
+        stringify(data, (err, output) => {
+            cb(output)
+        })
+    })
+}
 module.exports = {
-    create,
+    create, csv
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,9 +6,16 @@ routes.get('/', (req, res)=>{
     res.sendFile(__dirname+'/blog.html')
 })
 routes.post('/addLead', (req, res)=>{
-    const { name } = req.body
-    const lead = Lead.create({ name })
+    const { name, email } = req.body
+    const lead = Lead.create({ name, email })
     res.send('lead adicionado, ' + lead)
+})
+routes.get('/leads.csv', (req, res)=>{
+    res.setHeader('Content-Type', 'text/csv')
+    res.setHeader('Content-Disposition', 'attachment; filename=\"'+'leads.csv\"')
+    Lead.csv((data) =>{
+        res.send(data)
+    })
 })
 
 module.exports = routes
