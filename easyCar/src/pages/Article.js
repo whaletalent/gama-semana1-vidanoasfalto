ee7636ce54daa7099e3bff73a0d9d7c336698cc2
Pedro Sato <pedro.hiro@gmail.com>
2019-06-20 00:22:55 -0300

    fix

--- easyCar/src/pages/Article.js
+++ easyCar/src/pages/Article.js
@@ -15,7 +15,7 @@ class Article extends Component {
                 title: 'Dicas para evitar imprevistos durante a viagem',
                 subtitle: 'Fazer um check-up do seu carro é essencial para garantir uma viagem segura e sem imprevistos.',
                 textContent1: 'Mais um feriado se aproximando (20/08 - Corpus Christi) e milhões de pessoas usarão esses dias para descanso ou diversão. O vida no asfalto (VA) vai te ajudar a aproveitar esses momentos e curtir de forma mais segura.',
-                textContent1meio:'Para muitas pessoas serão 4 dias sem trabalhar, mas é importante que, antes de pegar a estrada, você cheque as condições de segurança do seu carro. Seja para perto ou para longe, fazer a revisão no seu veículo é essencial para garantir uma viagem tranquila. E para que você se lembre de tudo na hora da revisão, fizemos uma lista dos principais pontos para checar antes de pegar a estrada. Confira alguns itens básicos e dicas que às vezes deixamos passar batido:',
+                textContent1meio: 'Para muitas pessoas serão 4 dias sem trabalhar, mas é importante que, antes de pegar a estrada, você cheque as condições de segurança do seu carro. Seja para perto ou para longe, fazer a revisão no seu veículo é essencial para garantir uma viagem tranquila. E para que você se lembre de tudo na hora da revisão, fizemos uma lista dos principais pontos para checar antes de pegar a estrada. Confira alguns itens básicos e dicas que às vezes deixamos passar batido:',
                 topic1: 'Pneus',
                 textContent2: 'Ponto clássico de checagem, os pneus, têm fácil visualização, mas o VNA te dá três dicas importantes:',
                 topic2: 'Caso seu pneu fure, você pode não encontrar um borracheiro com tanta facilidade.',
@@ -46,19 +46,19 @@ class Article extends Component {
                     <h5 className="subtitle">{this.state.subtitle}</h5>
                 </div>
                 <div className="article-content">
-                    <p className="text-content">{this.state.textContent1}</p><br/>
-                    <p className="text-content">{this.state.textContent1meio}</p><br/>
+                    <p className="text-content">{this.state.textContent1}</p><br />
+                    <p className="text-content">{this.state.textContent1meio}</p><br />
                     <h2 className="supertopic">{this.state.topic1}</h2>
                     <p className="text-content">{this.state.textContent2}</p>
-                    <h5><span className="subtopic">1 - Não esqueça seu estepe!</span><br />{this.state.topic2}</h5>
+                    <h5><span className="subtopic">1 - Não esqueça seu estepe!</span><br /><span className="text-content">{this.state.topic2}</span></h5>
                     <h5><span className="subtopic">2 - Verifique a existência de bolhas.</span><br /><span className="text-content">{this.state.topic3}</span><br /><br /><img className="imagem" src="https://i.makeagif.com/media/7-29-2015/VERCQU.gif" /></h5>
                     <h5><span className="subtopic">3 - Seja justo com a banda de rodagem.</span><br /><span className="text-content">{this.state.topic4}</span><br /><br /><img className="imagem" src="https://i.makeagif.com/media/5-27-2014/jP2cO3.gif" /></h5>
-                    <h5 className="text-content"><span className="subtopic">Lembre-se:</span> <br />{this.state.textContent3}</h5><br/><br/>
+                    <h5 className="text-content"><span className="subtopic">Lembre-se:</span> <br />{this.state.textContent3}</h5><br /><br />
                     <img className="imagem" src="https://i.makeagif.com/media/6-09-2017/G-ouPi.gif" />
-                    <h2 className="supertopic"><span className="text-content">{this.state.topic5}</span></h2>
+                    <h2 className="supertopic">{this.state.topic5}</h2>
                     <p className="text-content">{this.state.textContent4}</p><br />
                     <h5><span className="subtopic">1 - Nas rodas de aço.</span><br /><span className="text-content">{this.state.topic3}</span></h5>
-                    <h5><span className="subtopic">2 - Nas rodas de liga leve.</span> <br /><span className="text-content">{this.state.topic3}</span></h5><br />
+                    <h5><span className="subtopic">2 - Nas rodas de liga leve.</span> <br /><span className="text-content">{this.state.topic4}</span></h5><br />
                     <h5 className="subtopic">Lembre-se: procure um borracheiro caso note alguma dessas características</h5>
                     <h2 className="supertopic">{this.state.topic8}</h2>
                     <span className="text-content">{this.state.textContent5}</span><br />
@@ -67,12 +67,12 @@ class Article extends Component {
                     <h2 className="topic">{this.state.topic9}</h2>
                     <span className="text-content">{this.state.textContent7}</span>
                     <h2 className="topic">{this.state.topic10}</h2>
-                    <span className="text-content">{this.state.textContent8}</span><br/>
+                    <span className="text-content">{this.state.textContent8}</span><br />
                     <span className="text-content">{this.state.topic11}</span>
                 </div>
-                <img className="div-bar" src={divBar}/>
+                <img className="div-bar" src={divBar} />
                 <h1 className="topic">Leia Também</h1>
-                <CompactPostCards/>
+                <CompactPostCards />
 
 
             </section >
