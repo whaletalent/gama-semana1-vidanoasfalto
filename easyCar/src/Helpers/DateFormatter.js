acaea20b84fa6b947fa5da0bc12329cd3d339391
LuanFerreira <luanfc@gmail.com>
2019-06-21 17:54:02 -0300

    Date Fix

--- easyCar/src/Helpers/DateFormatter.js
+++ easyCar/src/Helpers/DateFormatter.js
@@ -1,7 +1,6 @@
 const getDate = () =>{
     const date = new Date();
-    const formatted = `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2, "0")}-${(date.getDay().toString().padStart(2, "0"))} 
-         ${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}:${date.getSeconds().toString().padStart(2, "0")}`;
+    const formatted = `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2, "0")}-${(date.getDay().toString().padStart(2, "0"))} ${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}:${date.getSeconds().toString().padStart(2, "0")}`;
     return formatted
 }
 export default getDate()
